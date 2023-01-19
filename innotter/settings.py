8578871ad81f97c9c24addede59d8eead5ca530a
Knooki <knukknakiknak@gmail.com>
2023-01-19 22:33:54 +0300

    iib-5/created-access-token-validation-middleware-custom-exceptions

--- innotter/settings.py
+++ innotter/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CSRF_TRUSTED_ORIGINS = ["http://0.0.0.0:8000/"]
 
 CORS_ORIGIN_WHITELIST = tuple(env.list("CORS_ALLOWED_ORIGINS"))
-CORS_ALLOW_CREDENTIALS = True  # to accept cookies via ajax request
+CORS_ALLOW_CREDENTIALS = True # to accept cookies via ajax request
 
 ACCESS_PUBLIC = bytes(env.str("ACCESS_TOKEN_PUBLIC_KEY"), "utf-8")
 ACCESS_PRIVATE = bytes(env.str("ACCESS_TOKEN_PRIVATE_KEY"), "utf-8")
