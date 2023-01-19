40f0523bfd0f7b3938e2391fde4674d94104d321
Knooki <knukknakiknak@gmail.com>
2023-01-20 01:55:48 +0300

    iib-6/created-posts-models-and-migrations

--- innotter/settings.py
+++ innotter/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "authentication",
+    "posts",
 ]
 
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=80)
+    uuid = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+    tags = models.ManyToManyField('posts.Tag', related_name='pages')
+
+    owner = models.ForeignKey(
+        'accounts.User', on_delete=models.CASCADE, related_name='pages')
+    followers = models.ManyToManyField('accounts.User', related_name='follows')
+
+    image = models.URLField(null=True, blank=True)
+
+    is_private = models.BooleanField(default=False)
+    follow_requests = models.ManyToManyField(
+        'accounts.User', related_name='requests')
+
+    unblock_date = models.DateTimeField(null=True, blank=True)
+
+
+class Post(models.Model):
+    page = models.ForeignKey(
+        Page, on_delete=models.CASCADE, related_name='posts')
+    content = models.CharField(max_length=180)
+
+    reply_to = models.ForeignKey(
+        'posts.Post', on_delete=models.SET_NULL, null=True, related_name='replies')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
