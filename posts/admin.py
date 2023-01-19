f3a4aaeb6ddfc1ee7bb5982a04c3e414c0752e30
Knooki <knukknakiknak@gmail.com>
2023-01-20 02:38:51 +0300

    iib-6/registred-posts-models-added-simple-admin-classes

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Page, Post
+
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = (
+        "name",
+    )
+
+
+class PageAdmin(admin.ModelAdmin):
+    model = Page
+    list_display = (
+        "uuid",
+        "name",
+        "description",
+        "owner",
+        "image",
+        "is_private",
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "created_at",
+    )
+    model = Post
+    list_display = (
+        "title",
+        "page",
+        "content",
+        "reply_to",
+    )
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=30, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Page(models.Model):
     name = models.CharField(max_length=80)
@@ -22,15 +25,22 @@ class Page(models.Model):
         'accounts.User', related_name='requests')
 
     unblock_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=30, unique=True)
     page = models.ForeignKey(
         Page, on_delete=models.CASCADE, related_name='posts')
     content = models.CharField(max_length=180)
 
     reply_to = models.ForeignKey(
-        'posts.Post', on_delete=models.SET_NULL, null=True, related_name='replies')
+        'posts.Post', on_delete=models.SET_NULL, null=True, related_name='replies', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
