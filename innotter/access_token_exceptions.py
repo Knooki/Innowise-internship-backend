1cccbe3cfe6a15d7f695e8a66b4852940bc839cc
Knooki <knukknakiknak@gmail.com>
2023-01-24 16:20:27 +0300

    iib-5/linter

--- innotter/access_token_exceptions.py
+++ innotter/access_token_exceptions.py
@@ -4,12 +4,14 @@ from rest_framework.exceptions import APIException
 class AccessTokenExpired(APIException):
     status_code = 403
     default_detail = "Authentication access token has expired. Refresh it using refresh token on /refresh"
-    default_code = 'Access Token Expired'
+    default_code = "Access Token Expired"
 
 
 class InvalidAccessToken(APIException):
     status_code = 401
-    default_detail = "Access Token validation has failed. Please send valid access token."
+    default_detail = (
+        "Access Token validation has failed. Please send valid access token."
+    )
     default_code = "Access Token validation has failed"
 
 
@@ -17,6 +19,7 @@ class AccessTokenNotFound(APIException):
     status_code = 400
     default_detail = "Access Token not found. Please put it in Authorization header."
     default_code = "Access Token not found"
+
 
 class NoKeywordInAuthorization(APIException):
     status_code = 400
