8d45122ff1908893ed2cea50a02307bcb632a5be
Knooki <knukknakiknak@gmail.com>
2023-01-30 13:10:59 +0300

    iib-10/changes-to-accounts-app

---
+++ accounts/serializers/user_registration_serializer.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from ..models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    password_again = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password_again",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password_again": {"write_only": True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+        }
+
+    def save(self):
+        user = User(
+            email=self.validated_data["email"],
+            username=self.validated_data["username"],
+            first_name=self.validated_data["first_name"],
+            last_name=self.validated_data["last_name"],
+            role="user",
+            is_blocked=False,
+        )
+
+        password = self.validated_data["password"]
+        password_again = self.validated_data["password_again"]
+
+        if password != password_again:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        user.set_password(password)
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from .serializers.user_registration_serializer import UserRegistrationSerializer
+from .serializers.user_serializer import UserSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    User = get_user_model()
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    @action(detail=False, methods=["POST"], name="registration")
+    def registration(self, request) -> Response:
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
