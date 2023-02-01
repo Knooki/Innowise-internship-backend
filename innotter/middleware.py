dc11832c7c21eb52937604ce5c8fdeb275d54555
Knooki <knukknakiknak@gmail.com>
2023-02-02 01:00:56 +0300

    iib-9/changes-after-review

--- innotter/middleware.py
+++ innotter/middleware.py
@@ -16,8 +16,7 @@ from django.conf import settings
 
 class JWTMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        # if user is not authenticated
-        # now it is ["/", "/admin/.+"]
+
         if any(
             re.fullmatch(pattern, request.path)
             for pattern in settings.JWT_UNAUTHENTICATED_URL_PATTERNS
@@ -28,15 +27,17 @@ class JWTMiddleware(MiddlewareMixin):
         authorization_header_value = request.headers.get("Authorization")
 
         if not authorization_header_value:
-            return create_exception_response(AccessTokenNotFound)
+            raise (AccessTokenNotFound)
 
         if not re.fullmatch(settings.REGEX_BEARER, authorization_header_value):
-            return create_exception_response(BearerKeywordNotFound)
+            raise (BearerKeywordNotFound)
         try:
             access_token = authorization_header_value.split(" ")[-1]
-            payload = jwt.decode(access_token, settings.ACCESS_PUBLIC_KEY, algorithms=["RS256"])
+            payload = jwt.decode(
+                access_token, settings.ACCESS_PUBLIC_KEY, algorithms=["RS256"]
+            )
             return None
         except jwt.ExpiredSignatureError:
-            return create_exception_response(AccessTokenExpired)
+            raise (AccessTokenExpired)
         except (jwt.DecodeError, jwt.InvalidTokenError):
-            return create_exception_response(InvalidAccessToken)
+            raise (InvalidAccessToken)
